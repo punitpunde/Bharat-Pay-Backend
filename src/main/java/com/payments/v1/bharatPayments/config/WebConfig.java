5c0a55512c1c038db823ecfad50657669dae1738
Punit Punde <punitpunde@gmail.com>
2024-10-12 07:38:22 +0530

    deployed on railway

--- src/main/java/com/payments/v1/bharatPayments/config/WebConfig.java
+++ src/main/java/com/payments/v1/bharatPayments/config/WebConfig.java
@@ -1,4 +1,5 @@
 package com.payments.v1.bharatPayments.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("http://localhost:5173") // Replace with your React app's URL
+                .allowedOrigins("http://localhost:5173", "https://bharatpayui2.netlify.app") // Allow localhost and Netlify app
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
